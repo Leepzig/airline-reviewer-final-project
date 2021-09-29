fad3a1f69e027607344cf15832619c44c18ce6aa
andrew <neon.duncan@gmail.com>
2021-09-29 09:18:43 -0500

    setup basic gems, ran yarn install in client, and bundle install in backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email:"bobj@test.com", name: "Bob Junior", )
+User.create(email:"bobs@test.com", name: "Bob Senior", )
+User.create(email:"andrew@test.com", name: "Andrew", )
+
+Airline.create()
